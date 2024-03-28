be02a5967b465201c68269f0d905c9620235a672
Gaytri-Thakre <gaytrithakre8@gmail.com>
2024-03-28 11:59:27 +0530

    NavBar And Filter is created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import {filterData,apiUrl} from "./data";
+import Navbar from "./components/Navbar"
+import Filter from "./components/Filter"
+import Cards from "./components/Cards"
 const App = () => {
-  return <div>App</div>;
+  
+  return <div>
+    <Navbar></Navbar>
+    <Filter filterData = {filterData}></Filter>
+    <Cards></Cards>
+  </div>;
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+          <h1 className='flex justify-center align-center'>
+              Top Courses
+          </h1>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
